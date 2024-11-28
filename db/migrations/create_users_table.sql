1083d1039a10178ea23240e643079110be5a6712
ionnss <39104500+ionnss@users.noreply.github.com>
2024-11-28 15:49:53 -0300

    .

--- db/migrations/create_users_table.sql
+++ db/migrations/create_users_table.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     crp VARCHAR(20) UNIQUE NOT NULL,
-    theory_approach VARCHAR(100) NOT NULL,
+    theory_approach VARCHAR(100), --NOT NULL
     qualifications TEXT, --NOT NULL
-    user_role VARCHAR(20) NOT NULL,
+    user_role VARCHAR(20), --NOT NULL
     price_per_session NUMERIC(10, 2),
     sessions_availability TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
