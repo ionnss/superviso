97f3fd30e774bc0bdbc319743a5a5faa6442d386
ionnss <39104500+ionnss@users.noreply.github.com>
2024-11-28 13:50:54 -0300

    Updating DB tables and code structs

--- db/migrations/create_users_table.sql
+++ db/migrations/create_users_table.sql
@@ -2,12 +2,14 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     crp VARCHAR(20) UNIQUE NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    approach VARCHAR(100) NOT NULL,
-    user_type VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    active BOOLEAN DEFAULT TRUE
+    theory_approach VARCHAR(100) NOT NULL,
+    qualifications TEXT NOT NULL,
+    user_role VARCHAR(20) NOT NULL,
+    price_per_session NUMERIC(10, 2),
+    sessions_availability TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- models/user.go
+++ models/user.go
@@ -5,15 +5,17 @@ import "time"
 
 // User represents a user in the system
 type User struct {
-	ID           int       `json:"id"`
-	FirstName    string    `json:"firstname"`
-	LastName     string    `json:"lastname"`
-	Email        string    `json:"email"`
-	PasswordHash string    `json:"-"` // Omissão em respostas json
-	CRP          string    `json:"crp"`
-	CPF          string    `json:"cpf"`
-	Approach     string    `json:"approach"` // Abordagem clínica
-	UserType     string    `json:"usertype"` // Supervisando ou Supervisor
-	CreatedAt    time.Time `json:"createdat"`
-	Active       bool      `json:"active"`
+	ID                   int       `json:"id"`
+	FirstName            string    `json:"firstname"`
+	LastName             string    `json:"lastname"`
+	CPF                  string    `json:"cpf"`
+	Email                string    `json:"email"`
+	PasswordHash         string    `json:"-"` // Omissão em respostas json
+	CRP                  string    `json:"crp"`
+	TheoryApproach       string    `json:"approach"`          // Abordagem teórica
+	Qualifications       string    `json:"qualifications"`    // Qualificações do usuário
+	UserRole             string    `json:"usertype"`          // Supervisando ou Supervisor
+	PricePerSession      float64   `json:"price_per_session"` // Preço por sessão
+	SessionsAvailability string    `json:"availability"`      // Disponibilidade
+	CreatedAt            time.Time `json:"createdat"`
 }
