df29cab385e794c33a0ea840ff76f0c6da882ca9
ionnss <39104500+ionnss@users.noreply.github.com>
2024-11-28 22:42:41 -0300

    Tiny shit

--- api/sessions/auth.go
+++ api/sessions/auth.go
@@ -11,15 +11,20 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session, err := GetSession(r)
 		if err != nil {
+			log.Printf("Erro ao recuperar a sessão: %v\n", err)
 			http.Error(w, "Erro ao recuperar sessão", http.StatusInternalServerError)
 			return
 		}
 
 		log.Printf("Session Values: %+v\n", session.Values)
+
 		if session.Values["user_id"] == nil {
+			log.Println("Usuário não autenticado. Redirecionando para login.")
 			http.Error(w, "Usuário não autenticado", http.StatusUnauthorized)
 			return
 		}
+
+		log.Printf("Usuário autenticado com ID: %v\n", session.Values["user_id"])
 		next.ServeHTTP(w, r)
 	})
 }
