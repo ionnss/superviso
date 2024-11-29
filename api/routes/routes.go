f5c6095db194b43eca2f03e16e35d4e6a48aabce
ionnss <39104500+ionnss@users.noreply.github.com>
2024-11-29 01:16:51 -0300

    Frontend implementation

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 
 import (
 	"database/sql"
+	"net/http"
 	"superviso/api/sessions"
 	"superviso/api/supervisor"
 	"superviso/api/user"
@@ -11,9 +12,21 @@ import (
 )
 
 func ConfigureRoutes(r *mux.Router, db *sql.DB) {
+	// Servir arquivos estáticos em /assets/
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("view/assets/"))))
+
+	// Servir arquivos estáticos em /css/
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("view/css/"))))
+
+	// Rota raiz para servir o arquivo HTML
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "view/index.html")
+	}).Methods("GET")
+
 	// Rotas públicas
 	r.HandleFunc("/users/register", user.Register(db)).Methods("POST")
 	r.HandleFunc("/users/login", user.Login(db)).Methods("POST")
+	r.HandleFunc("/users/logout", sessions.Logout).Methods("POST")
 
 	// Rotas protegidas
 	protected := r.PathPrefix("/").Subrouter()
